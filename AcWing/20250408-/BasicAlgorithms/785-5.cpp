c2273bb483c36d1e86523546c851b95e5940ee42
Eagle233 <eagle233real@outlook.com>
2025-05-07 08:28:43 +0800

    202505070828

---
+++ AcWing/20250408-/BasicAlgorithms/785-5.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int N = 100010;
+
+int q[N];
+
+void quickSort(int l, int r) {
+    if (l >= r) {
+        return;
+    }
+
+    int i = l - 1;
+    int j = r + 1;
+    int mid = q[l + (r - l) / 2];
+
+    while (i < j) {
+        do {
+            i++;
+        } while (q[i] < mid);
+
+        do {
+            j--;
+        } while (q[j] > mid);
+
+        if (i < j) {
+            swap(q[i], q[j]);
+        }
+    }
+
+    quickSort(l, j);
+    quickSort(j + 1, r);
+}
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> q[i];
+    }
+
+    quickSort(0, n - 1);
+
+    for (int i = 0; i < n - 1; i++) {
+        cout << q[i] << ' ';
+    }
+
+    cout << q[n - 1] << endl;
+}
---
+++ AcWing/20250408-/BasicAlgorithms/786-6.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int N = 100010;
+
+int q[N];
+
+int quickSelect(int l, int r, int k) {
+    if (l >= r) {
+        return q[l];
+    }
+
+    int i = l - 1, j = r + 1;
+    int mid = q[l + (r - l) / 2];
+    while (i < j) {
+        do {
+            i++;
+        } while (q[i] < mid);
+
+        do {
+            j--;
+        } while (q[j] > mid);
+
+        if (i < j) {
+            swap(q[i], q[j]);
+        }
+    }
+
+    if (k <= j - l + 1) {
+        return quickSelect(l, j, k);
+    }
+    return quickSelect(j + 1, r, k - (j - l + 1));
+}
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, k;
+    cin >> n >> k;
+
+    for (int i = 0; i < n; i++) {
+        cin >> q[i];
+    }
+
+    cout << quickSelect(0, n - 1, k) << endl;
+}
